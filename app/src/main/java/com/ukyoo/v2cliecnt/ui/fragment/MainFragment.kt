816c941bd95471979253b8d24c66c95183fd858f
ukyo6 <he7212wei@gmail.com>
2018-11-13 17:32:26 +0800

    add desc

--- app/src/main/java/com/ukyoo/v2cliecnt/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/ukyoo/v2cliecnt/ui/fragment/MainFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ukyoo.v2cliecnt.R
 
+/**
+ * 主页
+ */
 class MainFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
