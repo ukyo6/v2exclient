921d25978a2d3ae482678d67da6b443bafd607ba
ukyo6 <he7212wei@gmail.com>
2018-12-10 20:05:27 +0800

    nodeFragment请求

--- app/src/main/java/com/ukyoo/v2client/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/ukyoo/v2client/ui/detail/DetailActivity.kt
@@ -1,25 +1,46 @@
 package com.ukyoo.v2client.ui.detail
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import com.ukyoo.v2client.R
 import com.ukyoo.v2client.base.BaseActivity
 import com.ukyoo.v2client.databinding.ActivityDetailBinding
+import com.ukyoo.v2client.entity.TopicModel
+import com.ukyoo.v2client.ui.viewmodels.DetailViewModel
 
 
 class DetailActivity : BaseActivity<ActivityDetailBinding>() {
-    private var mTopicId:Int = 0
+    private var mTopicId: Int? = 0
+    private lateinit var model: TopicModel
+
+    //get viewModel by di
+    private val viewModel by lazy {
+        getInjectViewModel<DetailViewModel>()
+    }
 
     override fun restoreArgs(savedInstanceState: Bundle?) {
         super.restoreArgs(savedInstanceState)
+        mTopicId = savedInstanceState?.getInt("topic_id",-1)
     }
 
     override fun saveArgs(outState: Bundle?) {
         super.saveArgs(outState)
-        outState?.putInt("topic_id", mTopicId)
+        mTopicId?.let { outState?.putInt("topic_id", it) }
     }
 
     override fun loadData(isRefresh: Boolean) {
+        if (intent.getStringExtra("topic_id") != null) {
+            mTopicId = intent.getIntExtra("topic_id", -1)
+
+            viewModel.getReplies()
+        } else if (intent.getParcelableExtra<TopicModel>("model") != null) {
+            model = intent.getParcelableExtra("model")
+            mTopicId = model.id
+
+            viewModel.getTopicAndReplies()
+        }
+
+
+
     }
 
     override fun initView() {
@@ -28,13 +49,5 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>() {
 
     override fun getLayoutId(): Int {
         return R.layout.activity_detail
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
-        super.onSaveInstanceState(outState, outPersistentState)
     }
 }
---
+++ app/src/main/java/com/ukyoo/v2client/ui/viewmodels/DetailViewModel.kt
@@ -0,0 +1,15 @@
+package com.ukyoo.v2client.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel: ViewModel(){
+
+    fun getReplies(): Any? {
+        return null
+    }
+
+    fun getTopicAndReplies(): Any? {
+        return null
+    }
+
+}
